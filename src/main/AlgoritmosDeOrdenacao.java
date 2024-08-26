9ca1b4c150ce4de605b60b5a493f33e8a1512fcc
Kauã <gamersnes69@gmail.com>
2024-08-26 19:56:00 -0300

    Created new class and two new methods for ordenation

---
+++ src/main/AlgoritmosDeOrdenacao.java
@@ -0,0 +1,20 @@
+package main;
+
+public class AlgoritmosDeOrdenacao {
+
+    public void bubbleSort (int lista[]){
+        for (int i = 0; i < lista.length; i++){
+            for (int j = 0; j < lista.length - 1; j++){
+                if (lista[j] > lista[j + 1]){
+                    swap(lista, j, j+1);
+                }
+            }
+        }
+    }
+
+    private void swap(int lista[], int a, int b){
+        int aux = lista[a];
+        lista[a] = lista[b];
+        lista[b] = aux;
+    }
+}
