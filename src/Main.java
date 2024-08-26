7cb0d4e3b359b128aa8395f43b63b11d3f45c065
Kauã <gamersnes69@gmail.com>
2024-08-26 19:06:04 -0300

    changed the language used in the project for college reasons.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import classes.Movie;
+import classes.Filme;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,23 +6,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie("Jurassic Park", 9, 1993);
-        Movie movie2 = new Movie("Jurassic World", 8, 2015);
-        Movie movie3 = new Movie("Jurassic World: Dominion",7, 2022);
+        Filme filme1 = new Filme("Jurassic Park", 9, 1993);
+        Filme filme2 = new Filme("Jurassic World", 8, 2015);
+        Filme filme3 = new Filme("Jurassic World: Dominion",7, 2022);
 
-        Movie[] movies = {movie1, movie2, movie3};
+        Filme[] filmes = {filme1, filme2, filme3};
 
-        List<Movie> list = new ArrayList<>();
+        List<Filme> lista = new ArrayList<>();
 
-        for (Movie m : movies){
-            list.add(m);
+        for (Filme m : filmes){
+            lista.add(m);
         }
 
-        System.out.println("Original order: " +
-                            list);
+        System.out.println("Ordem original: " +
+                            lista);
 
-        System.out.println("Organized order: ");
-        Collections.sort(list);
-        System.out.println(list);
+        System.out.println("Ordem organizada: ");
+        Collections.sort(lista);
+        System.out.println(lista);
         }
     }
---
+++ src/classes/Filme.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Filme implements Comparable<Filme> {
+    private String nome;
+    private int nota;
+    private int ano;
+
+    public Filme(String nome, int nota, int ano) {
+        this.nome = nome;
+        this.nota = nota;
+        this.ano = ano;
+    }
+
+    public String toString(){
+        return "Name: " + this.nome + " (" + this.ano + ")" + " Score: " + this.nota;
+    }
+
+    @Override
+    public int compareTo(Filme m) {
+        if (this.nota != m.nota){
+            return Integer.compare(m.nota, this.nota);
+        }
+        else if (this.ano != m.ano) {
+            return Long.compare(this.ano, m.ano);
+        }
+        else {
+            return this.nome.compareTo(m.nome);
+        }
+    }
+}
