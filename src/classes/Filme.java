d688a1ce4e00250cc3b4f0654d11f3ff88428bc8
αи∂яє ℓ uαи <andreluanbq@gmail.com>
2024-09-02 17:31:04 -0300

    add titulos e subtitulos

--- src/classes/Filme.java
+++ src/classes/Filme.java
@@ -10,8 +10,8 @@ public class Filme implements Filme_IF {
     private int nota;
     private int ano;
 
-    private static final String[] Titulos = {"Era do Gelo: ", "Piratas do Caribe: ", "Rei Leão: ", "Velozes e Furiosos: ", "Mad Max: ", "O Vendedor de Linguiça: ", "Os Vingadores: ", "Tropa de Elite: ", "Cidade de Deus: ", "JheffersonKauã: ", "Transformers: ", "Ratatouille: ", "Pitú: ", "Os Trapalhões: ", "Os Minions: ", "Herbie: ","Os cara de pau: " };
-    private static final String[] SubTitulos = {"A História de uma abelha", "A era de ultron", "A vingança dos derrotados", "O lado escuro da lua", "Desafio na UEPB", "uma noite muito louca", "Sexo com Proteção", "Prisioneiro de Azkaban", "Câmara Secreta", "A Pedra Filosofal", "Trair é errado", "Meu fusca turbinado" , "e os 7 anões", "Guerra infinita"};
+    private static final String[] Titulos = {"Era do Gelo: ", "Piratas do Caribe: ", "Rei Leão: ", "Velozes e Furiosos: ", "Mad Max: ", "O Vendedor de Linguiça: ", "Os Vingadores: ", "Tropa de Elite: ", "Cidade de Deus: ", "JheffersonKauã: ", "Transformers: ", "Ratatouille: ", "Pitú: ", "Os Trapalhões: ", "Os Minions: ", "Herbie: ","Os cara de pau: ","Carros: ","John wick","Resident evil: ","Diário de um banana: ", "Kratos: ", "Hulk: ", "Vida de Inseto: ", "La La Land: ","Lobo de Wall street: ", "O Regresso: ", "Titanic: ","Homem Aranha: "};           
+    private static final String[] SubTitulos = {"A História de uma abelha", "A era de ultron", "A vingança dos derrotados", "O lado escuro da lua", "Desafio na UEPB", "uma noite muito louca", "Sexo com Proteção", "Prisioneiro de Azkaban", "Câmara Secreta", "A Pedra Filosofal", "Trair é errado", "Meu fusca turbinado" , "e os 7 anões", "Guerra infinita","o Deus da guerra","","e a mãe", "cantando estações", "de volta ao lar"," longe de casa", "sem volta pra casa"};      
 
     public Filme(String nome, int nota, int ano) {
         this.nome = nome;
